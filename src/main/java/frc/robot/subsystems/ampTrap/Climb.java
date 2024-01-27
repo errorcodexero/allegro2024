68d6d2dd3046b6bb4c96074f42d92aebb8935bd2
Michael <107226999+themehdev@users.noreply.github.com>
2024-01-27 11:47:37 -0800

    created the action

---
+++ src/main/java/frc/robot/subsystems/ampTrap/Climb.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.ampTrap;
+
+import org.xero1425.base.actions.Action;
+import org.xero1425.base.subsystems.motorsubsystem.MotorEncoderPowerAction;
+
+public class Climb extends Action{
+    private AmpTrapSubsystem sub_;
+    private MotorEncoderPowerAction
+}
