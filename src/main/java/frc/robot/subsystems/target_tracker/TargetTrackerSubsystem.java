29e0abda5d0061ec3177f41123f0640b8072c153
Butch Griffin <butchg@comcast.net>
2024-01-31 07:24:43 -0800

    minor cleanup after merges

--- src/main/java/frc/robot/subsystems/target_tracker/TargetTrackerSubsystem.java
+++ src/main/java/frc/robot/subsystems/target_tracker/TargetTrackerSubsystem.java
@@ -16,11 +16,13 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public class TargetTrackerSubsystem extends Subsystem {
 
-    Pose2d target_pos_ = new Pose2d() ;
-    Pose2d robot_pos_ = new Pose2d() ;
-    AllegroRobot2024 robot_= (AllegroRobot2024)getRobot().getRobotSubsystem(); 
+    public TargetTrackerSubsystem(Subsystem parent) {
+        super(parent, "targettracker") ;
+        Pose2d target_pos_ = new Pose2d() ;
+        Pose2d robot_pos_ = new Pose2d() ;
+        AllegroRobot2024 robot_= (AllegroRobot2024)getRobot().getRobotSubsystem(); 
 
-    robot_pos_ = robot_.getSwerve().getPose(); 
-
+        robot_pos_ = robot_.getSwerve().getPose(); 
+    }
 }
   
