1086bebf955fe31db3f22bcd9c944db22c39f8e4
Michael <107226999+themehdev@users.noreply.github.com>
2024-01-31 11:23:04 -0800

    Added stow climber to stow and updated the prepclimb so the code isnt terrible anymore

--- src/main/java/frc/robot/subsystems/amp_trap/AmpTrapStowAction.java
+++ src/main/java/frc/robot/subsystems/amp_trap/AmpTrapStowAction.java
@@ -13,6 +13,7 @@ public class AmpTrapStowAction extends Action{
     private MotorEncoderPowerAction stow_roller_;
     private MCMotionMagicAction stow_pivot_;
     private MCMotionMagicAction stow_wrist_;
+    private MCMotionMagicAction stow_climber_;
     
 
     public AmpTrapStowAction(AmpTrapSubsystem sub) throws Exception {
@@ -24,6 +25,7 @@ public class AmpTrapStowAction extends Action{
         stow_roller_ = new MotorEncoderPowerAction(sub_.getManipulator(), 0);
         stow_pivot_ = new MCMotionMagicAction(sub_.getArm(), "pids:position" , "targets:stow" , 0.5 , 1);
         stow_wrist_ = new MCMotionMagicAction(sub_.getWrist(), "pids:position" , "targets:stow" , 0.5 , 1);
+        stow_climber_ = new MCMotionMagicAction(sub_.getClimber(), "pids:position", "targets:stow", 0.5, 1);
     }
 
     @Override
@@ -34,14 +36,15 @@ public class AmpTrapStowAction extends Action{
         sub_.getManipulator().setAction(stow_roller_, true);
         sub_.getArm().setAction(stow_pivot_, true);
         sub_.getWrist().setAction(stow_wrist_, true);
-        setDone();
+        sub_.getClimber().setAction(stow_climber_, true);
+        //setDone();
     }
 
     @Override
     public void run() throws Exception {
         super.run() ;
 
-        if (stow_elevator_.isDone() && stow_pivot_.isDone() && stow_wrist_.isDone()) {
+        if (stow_elevator_.isDone() && stow_pivot_.isDone() && stow_wrist_.isDone() && stow_climber_.isDone()) {
             setDone();
         }
 
