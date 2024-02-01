d56bd4254844cb44c4046b0398de808d1dff6e35
NAMAN1034 <155015788+NAMAN1034@users.noreply.github.com>
2024-01-31 19:05:28 -0800

    Update NoteTransferAction.java
    Another update to notetransferaction. Not sure of what speed feeder wheels should be. Feedback encouraged

--- src/main/java/frc/robot/subsystems/intake_shooter/NoteTransferAction.java
+++ src/main/java/frc/robot/subsystems/intake_shooter/NoteTransferAction.java
@@ -1,7 +1,14 @@
-package frc.main.subsystems.intake_shooter;
-
-import org.xero1425.actions.Action;
+package frc.robot.subsystems.intake_shooter;
 
 public class NoteTransferAction extends Action{
+    private IntakeShooterSubsystem sub_;
+    private MotorEncoderPowerAction feeder_;
+
+    public NoteTransferAction(IntakeShooterSubsystem sub) throws Exception{
+        super(sub.getRobot().getMessageLogger());
+
+        sub = sub_;
+        feeder = new MotorEncoderPowerAction(sub.getFeeder(), 2);
+    }
     
 }
