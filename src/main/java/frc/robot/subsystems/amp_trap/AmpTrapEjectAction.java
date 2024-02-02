6033b73adf3bd469cfec539ee85c652899c75682
amukh0 <153405984+amukh0@users.noreply.github.com>
2024-02-01 19:14:53 -0800

    Created AmpTrap Eject

---
+++ src/main/java/frc/robot/subsystems/amp_trap/AmpTrapEjectAction.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems.amp_trap;
+
+
+import org.xero1425.base.actions.Action;
+import org.xero1425.base.subsystems.motorsubsystem.MCMotionMagicAction;
+import org.xero1425.base.subsystems.motorsubsystem.MotorEncoderPowerAction;
+
+public class AmpTrapEjectAction extends Action{
+    
+    private AmpTrapSubsystem sub_;
+    private MCMotionMagicAction eject_elevator_;
+    private MCMotionMagicAction eject_arm_;
+    private MCMotionMagicAction eject_wrist_;
+    private MotorEncoderPowerAction eject_manipulator_;
+    
+    
+    public AmpTrapEjectAction(AmpTrapSubsystem sub) throws Exception {
+
+        super(sub.getRobot().getMessageLogger());
+
+
+        sub_ = sub;
+        eject_elevator_ = new MCMotionMagicAction(sub_.getElevator(), "pids:position" , "targets:stow" , 0.5 , 1);
+        eject_arm_ = new MCMotionMagicAction(sub_.getArm(), "pids:position" , "targets:stow" , 0.5 , 1);
+        eject_wrist_ = new MCMotionMagicAction(sub_.getWrist(), "pids:position" , "targets:stow" , 0.5 , 1);
+        eject_manipulator_ = new MotorEncoderPowerAction(sub_.getManipulator(), 1, 5);
+    }
+
+    @Override 
+    public void start() throws Exception{
+        super.start();
+
+        sub_.getElevator().setAction(eject_elevator_, true);
+        sub_.getArm().setAction(eject_elevator_, true);
+        sub_.getWrist().setAction(eject_wrist_, true);
+        sub_.getManipulator().setAction(eject_manipulator_, true);
+    }
+
+    @Override
+    public void run() throws Exception {
+        super.run() ;
+
+        if (eject_elevator_.isDone() && eject_arm_.isDone() && eject_wrist_.isDone() && eject_manipulator_.isDone()) {
+            setDone();
+        }
+
+    }
+
+    @Override
+    public String toString(int indent) {
+        return spaces(indent) + "AmpTrapEjectAction";
+    }
+}
+
+
