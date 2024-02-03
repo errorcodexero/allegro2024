25c75b99910e8b70fae517c90698955fb14c66cd
Naman Pradhan <s2gnaman@gmail.com>
2024-02-03 22:28:00 +0000

    Fixed Build issues with PrepNoteTransfer and NoteTransfer action

--- src/main/java/frc/robot/subsystems/intake_shooter/PrepNoteTransferAction.java
+++ src/main/java/frc/robot/subsystems/intake_shooter/PrepNoteTransferAction.java
@@ -15,32 +15,29 @@ public class PrepNoteTransferAction extends Action{
 
         sub = sub_;
         //detects if updown is in collecting state so it can prepare the feeder wheels
-        if(updown_ = new MCMotionMagicAction(sub.getUpDown(), "pids:position", "targets:collect", 0.5, 0.5)) {
-            feeder_ = new MotorEncoderPowerAction(sub.getFeeder(), 1);
-        }
+        //if(updown_ = new MCMotionMagicAction(sub.getUpDown(), "pids:position", "targets:collect", 0.5, 0.5)) {
+        //    feeder_ = new MotorEncoderPowerAction(sub.getFeeder(), 1);
+        //}
     }
 
     @Override
     public void start() throws Exception {
-        super.start()
+        super.start();
         sub_.getFeeder().setAction(feeder_, true);
         sub_.getUpDown().setAction(updown_, true);
-        
-        
     }
 
     @Override
     public void run() throws Exception{
         super.run() ;
 
-        if (feeder_.isDone() && UpDown_.isDone()) {
-            setDone();
-        }
+        //if (feeder_.isDone() && UpDown_.isDone()) {
+        //    setDone();
+        //}
     }
 
     @Override
     public String toString(int indent){
-        return spaces(indent) + "NoteTransferAction";
-    }
-    
+        return spaces(indent) + "PrepNoteTransferAction";
+    }   
 }
