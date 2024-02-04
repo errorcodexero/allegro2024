a2fb316b0036deacd48a9ad31178a2ca141a013e
prankX02 <151118750+prankX02@users.noreply.github.com>
2024-02-03 22:26:08 -0800

    Updated StartCollectAction with reviews

--- src/main/java/frc/robot/subsystems/intake_shooter/StartCollectAction.java
+++ src/main/java/frc/robot/subsystems/intake_shooter/StartCollectAction.java
@@ -8,7 +8,7 @@ import org.xero1425.base.subsystems.motorsubsystem.MCVelocityAction;
 //code and has a second part, FinishCollectAction. This uses the updown, tilt and feeder motors. This has a state machine which makes 
 //tilt motor move first before the updown brings the intake down.
 
-public class StartCollectAction extends Action{
+public class StartCollectAction extends Action {
  
     private enum State {
         Idle,
@@ -27,30 +27,34 @@ public class StartCollectAction extends Action{
 
     private State state_;
 
-    public StartCollectAction(IntakeShooterSubsystem sub)throws Exception {
+    public StartCollectAction(IntakeShooterSubsystem sub) throws Exception 
+ {
         super(sub.getRobot().getMessageLogger());
-        //switches the state to Idle at first where it doewsn't do anything
+        //switches the state to Idle at first where it doesn't do anything
         state_ = State.Idle;
 
         sub_ = sub;
         //Initializing the upDown and tilt collect actions with the Motion Magic constructor
-        collect_updown_action = new MCMotionMagicAction(sub.getUpDown(), "updown_position", "targets:collect",0.5, 0.5);
+        collect_updown_action = new MCMotionMagicAction(sub.getUpDown(), "pids:position", "targets:updown_collect_position",0.5, 0.5);
+        collect_tilt_action = new MCMotionMagicAction(sub.getTilt(), "pids:position", "targets:tilt_collect_position", 0,0 );
 
-        collect_tilt_action = new MCMotionMagicAction(sub.getTilt(), "tilt_position", "targets:collect", 0,0 );
         //Initializing the feeder collect action with the velocity action
-        collect_feeder_action = new MCVelocityAction(sub.getFeeder(), "feeder_velocity", "targets:collect");
+        collect_feeder_action = new MCVelocityAction(sub.getFeeder(), "pids:velocity", "targets:feeder_collect_velocity");
 
     }
     @Override
-    public void start() throws Exception {
+    public void start() throws Exception 
+ {
         super.start();
         //Begins to move the Tilt upwards to a safe position. Also begins spinning the feeder motor
         state_ = State.TiltUp;
         sub_.getFeeder().setAction(collect_feeder_action, true);
         sub_.getTilt().setAction(collect_tilt_action, true);
+
    }
     @Override
-    public void run() throws Exception{
+    public void run() throws Exception
+ {
         super.run();
        //Defines the sequence of each state and what to do in these states
         switch(state_) {
@@ -68,17 +72,19 @@ public class StartCollectAction extends Action{
     public String toString(int indent) {
         return prefix(indent) + "StartCollectAction";
     }
-    private void stateTiltUp() {
-       //Checks if the Tilt motor has reached the safe position. If it has, switch to next state and begin moving the intake down
+    private void stateTiltUp()
+ {
+       //Checks if the Tilt motor has reached the safe position. If it has, switch to the next state and begin moving the intake down
         if (!sub_.getTilt().isBusy()) {
             state_ = State.WaitForNote;
              sub_.getUpDown().setAction(collect_updown_action, true);
-             sub_.getTilt().setAction(collect_tilt_action, true);
+
 
         }
     }
-    private void stateWaitForNote() {
-        //This uses the sensor and keeps checking until the sensor detects a note is in place
+    private void stateWaitForNote() 
+ {
+        //Uses the sensor to detect if a note is in place. If it is, then stop the feeder and end everything.
         if (sub_.isNotePresent()) {
             sub_.getFeeder().cancelAction();
             state_ = State.Idle;
