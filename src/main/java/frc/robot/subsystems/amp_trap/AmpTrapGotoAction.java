da608a023f03f1167e2cc4deb0959cc0d29a1d12
Michael <107226999+themehdev@users.noreply.github.com>
2024-02-14 14:54:35 -0800

    Updated to meet butch's requirements, and made it faster supposedly

--- src/main/java/frc/robot/subsystems/amp_trap/AmpTrapGotoAction.java
+++ src/main/java/frc/robot/subsystems/amp_trap/AmpTrapGotoAction.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.amp_trap;
 import org.xero1425.base.actions.Action;
 import org.xero1425.base.subsystems.motorsubsystem.MCMotionMagicAction;
 
+
+
 public class AmpTrapGotoAction extends Action {
     private MCMotionMagicAction elevatorThreshAction;
     private MCMotionMagicAction elevatorGotoAction;
@@ -12,7 +14,15 @@ public class AmpTrapGotoAction extends Action {
     private double armNoGoZoneLower;
     private double armNoGoZoneUpper;
     private double armTarget;
-    private String state;
+    private double elevatorTarget;
+    private enum State {
+        Goto,
+        ThreshGoto,
+        ArmGoto,
+        Thresh
+    };
+
+    private State state;
 
     public AmpTrapGotoAction(AmpTrapSubsystem subsystem, String elevatorTarget, String armTarget) throws Exception{
         this(subsystem, subsystem.getSettingsValue(elevatorTarget).getDouble(), subsystem.getSettingsValue(armTarget).getDouble());
@@ -24,9 +34,10 @@ public class AmpTrapGotoAction extends Action {
         // Get the elevator threshold and the no-go zone limits for the arm from the params file
         this.elevatorThreshold = subsystem.getElevator().getSettingsValue("threshold").getDouble();
         this.armNoGoZoneLower = subsystem.getArm().getSettingsValue("no-go-zone:lower").getDouble();
-        this.armNoGoZoneUpper = subsystem.getSettingsValue("no-go-zone:upper").getDouble();
+        this.armNoGoZoneUpper = subsystem.getArm().getSettingsValue("no-go-zone:upper").getDouble();
 
         this.armTarget = armTarget;
+        this.elevatorTarget = elevatorTarget;
 
         this.elevatorThreshAction = new MCMotionMagicAction(subsystem.getElevator(), "ElevatorThresh", elevatorThreshold, 0.01, 0.02);
         this.elevatorGotoAction = new MCMotionMagicAction(subsystem.getArm(), "ElevatorThresh", elevatorTarget, 0.01, 0.02);
@@ -34,7 +45,7 @@ public class AmpTrapGotoAction extends Action {
         // Create the MCMotionMagicAction for the arm motor
         this.armAction = new MCMotionMagicAction(subsystem.getArm(), "ArmGoto", armTarget, 3, 10);
 
-        state = "Thresh";
+        state = State.Thresh;
     }
 
     @Override
@@ -45,13 +56,20 @@ public class AmpTrapGotoAction extends Action {
         subsystem.getArm().setAction(armAction);
 
         // Start the MCMotionMagicActions
-        if(((armTarget < armNoGoZoneLower && armTarget > armNoGoZoneUpper) && !(armTarget > armNoGoZoneUpper && subsystem.getArm().getPosition() < armNoGoZoneLower)) || ((armTarget > armNoGoZoneUpper && subsystem.getArm().getPosition() > armNoGoZoneUpper) || (armTarget < armNoGoZoneLower && subsystem.getArm().getPosition() < armNoGoZoneLower))){
+        // If arm target isn't in no-go-zone
+        // and the arm target and arm actual aren't on opposite sides
+        // then they can be run in paralell
+        if(((armTarget < armNoGoZoneLower || armTarget > armNoGoZoneUpper) && (!(armTarget > armNoGoZoneUpper && subsystem.getArm().getPosition() < armNoGoZoneLower) || !(armTarget < armNoGoZoneLower && subsystem.getArm().getPosition() > armNoGoZoneUpper)))){
             subsystem.getArm().setAction(armAction);
             subsystem.getElevator().setAction(elevatorGotoAction);
-            state = "Goto";
+            state = State.Goto;
+        }else if (elevatorTarget > elevatorThreshold){
+        // Otherwise, start the 
+            subsystem.getElevator().setAction(elevatorGotoAction);
+            state = State.ThreshGoto;
         }else{
             subsystem.getElevator().setAction(elevatorThreshAction);
-            state = "Thresh";
+            state = State.Thresh;
         }
     }
 
@@ -59,29 +77,34 @@ public class AmpTrapGotoAction extends Action {
     public void run() throws Exception{
         super.run();
         // Update the MCMotionMagicActions
-        if(state.equals("Goto")){
+        if(state == State.Goto || state == State.ThreshGoto){
             elevatorGotoAction.run(); 
         }
-        if(state.equals("Goto") || state.equals("ArmGoto")){
+        if(state == State.Goto || state == State.ArmGoto){
             armAction.run();
         }
-        if(state.equals("Thresh")){
+        if(state == State.Thresh){
             elevatorThreshAction.run();
         }
-        // If the elevator has moved up past the elevator threshold, move the arm to its target position
-        if (elevatorThreshAction.isDone() && state.equals("Thresh")){
+
+        if(state == State.ThreshGoto && subsystem.getElevator().getPosition() > elevatorThreshold){
             subsystem.getArm().setAction(armAction);
-            state = "ArmGoto";
+            state = State.Goto;
+        }
+        // If the elevator has moved up past the elevator threshold, move the arm to its target position
+        if (elevatorThreshAction.isDone() && state == State.Thresh){
+            subsystem.getArm().setAction(armAction);
+            state =  State.ArmGoto;
         }
 
         if(armAction.isDone()){
-            if(elevatorGotoAction.isDone() && state.equals("Goto")){
+            if(elevatorGotoAction.isDone() && state == State.Goto){
                 setDone();
                 return;
             }
-            if(state.equals("ArmGoto")){
+            if(state == State.ArmGoto){
                 subsystem.getElevator().setAction(elevatorGotoAction);
-                state = "Goto";
+                state = State.Goto;
             }
         }
     }
