57ddf61795c05b5131dc0b9286b285babcb8388f
Mark Kassab <mark.a.kassab@gmail.com>
2024-02-24 15:25:42 -0800

    Partial shooter re-tuning. Up to and including 2.89m (previously 3.22m).

--- src/main/java/frc/robot/subsystems/intake_shooter/IntakeAutoShootAction.java
+++ src/main/java/frc/robot/subsystems/intake_shooter/IntakeAutoShootAction.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.intake_shooter;
 
 import org.xero1425.base.actions.Action;
 import org.xero1425.base.misc.XeroTimer;
+import org.xero1425.base.subsystems.Subsystem.DisplayType;
 import org.xero1425.base.subsystems.motorsubsystem.MCTrackPosAction;
 import org.xero1425.base.subsystems.motorsubsystem.MCVelocityAction;
 import org.xero1425.base.subsystems.motorsubsystem.MotorEncoderPowerAction;
